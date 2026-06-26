c217d28c3a66a0077f54740985e7a66a4a80ed37
RSherathiya <ravi.sherathiya@excellentwebworld.in>
2026-06-26 11:05:42 +0530

    feat: initialize express server with root and about routes

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("Hello World!");
+import express from "express"
+
+const app = express();
+
+
+app.get("/", (req, res) => {
+    res.send("Hello World!!!!!!!!!!!!!!!!")
+})
+
+app.get("/about", (req, res) => {
+    res.send("This is about page")
+})
+
+app.listen(3000, () => {
+    console.log("Server started at http://localhost:3000")
+})
